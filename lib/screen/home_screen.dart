5450d7ba1d74e61d583bf584684255fb18a989c2
jgoo1 <jgoo1@naver.com>
2022-11-27 17:09:27 +0900

    Column 적용 연습

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,8 +6,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen2'),
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Container(
+                color: Colors.red,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.orange,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Container(
+                color: Colors.green,
+                width: 50.0,
+                height: 50.0,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
